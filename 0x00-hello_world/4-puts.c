03bd22c994e7522b3ad32aa1312b52af50ffe62c
Nnaemeka Osuagwu-Nwogu <Nnaemeka.osuagwu.n@gmail.com>
2023-06-15 08:13:52 -0700

    Print a string using the puts() function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - A program that prints a string using puts() function
+ *
+ */
+
 int main(void)
 {
-  char string_out[] = "Programming is like building a multilingual puzzle";
+  char string_out[] = "\"Programming is like building a multilingual puzzle";
   puts(string_out);
 
   return (0);
