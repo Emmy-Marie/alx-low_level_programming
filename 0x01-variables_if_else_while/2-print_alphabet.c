e23e49e26796ebdaeda5943344abe644d3968aa5
Nnaemeka Osuagwu-Nwogu <Nnaemeka.osuagwu.n@gmail.com>
2023-06-16 08:11:57 -0700

    Print lowercase alphabets using putchar() function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabets in lowercase
+ * Return:0 (success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter = letter + 1;
+	}
+	return (0);
+}
