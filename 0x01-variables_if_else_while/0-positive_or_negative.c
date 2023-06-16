c0e1442ff7b88b4084b848633e77b37c174d54b9
Nnaemeka Osuagwu-Nwogu <Nnaemeka.osuagwu.n@gmail.com>
2023-06-16 05:14:57 -0700

    Using if...else conditional statement in C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,11 +10,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n = 0) {
+	if (n == 0)
+	{
 		printf("%d is zero", n);
-	} else if (n > 0) {
+	} else if (n > 0)
+	{
 		printf("%d is positive", n);
-	} else {
+	} else
+	{
 		printf("%d is negative", n);
 	}
 	return (0);
