9160d00b887b405a4062a3b8d84a568ca16a0eb4
Nnaemeka Osuagwu-Nwogu <Nnaemeka.osuagwu.n@gmail.com>
2023-06-16 10:29:37 -0700

    Print single digit base 10 numbers using putchar()

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print single base 10 digits using putchar function
+ * Return:0 (success)
+ */
+
+int main(void)
+{
+	int i, c;
+
+	c = i + '0'
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
