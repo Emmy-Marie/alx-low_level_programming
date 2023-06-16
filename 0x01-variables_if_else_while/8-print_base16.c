8c594c18015bd07dc0216e65d85ae650c4225f4c
Nnaemeka Osuagwu-Nwogu <Nnaemeka.osuagwu.n@gmail.com>
2023-06-16 11:24:02 -0700

    Print all base 16 numbers in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all base 16 numbers in lowercase
+ * Return:0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 16)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+			i++;
+		} else
+		{
+			putchar(i + 'a');
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
