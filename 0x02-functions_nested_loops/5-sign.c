e96eff4ee93423ce63d73d788587e1064c85cd38
Nnaemeka Osuagwu-Nwogu <nnaemeka.osuagwu.n@gmail.com>
2023-06-20 16:17:47 +0000

    Print the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * @n: The argument is an integer
+ * Return: 1 if greater than zero, 0 if zero and -1 if less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+	}
+	else
+	{
+		_putchar('-');
+	}
+}
