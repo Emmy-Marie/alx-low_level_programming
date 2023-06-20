b8be4f68ebe80368209710d728c121d8e13c836c
Nnaemeka Osuagwu-Nwogu <nnaemeka.osuagwu.n@gmail.com>
2023-06-20 11:57:39 +0000

    Print _putchar followed by new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,8 @@
 /**
  * main - A program that prints the word "_putchar"
  * Return:0 (Success)
- * _putchar is already declared
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
 */
 
 int _putchar(char c);
