e22ca15eaffb85da849abab90c0b0d3a85eb9d3f
Nnaemeka Osuagwu-Nwogu <nnaemeka.osuagwu.n@gmail.com>
2023-06-21 03:56:41 +0000

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @i: - First integer
+ * @j: - Second integer
+ * Return: Add two integers and return the result
+ */
+
+int add(int i, int j)
+{
+	int a;
+
+	a = i + j;
+	_putchar(a + '0');
+}
