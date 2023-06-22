6b81a272e8b391a8632433bdaddd39902621e0ab
Nnaemeka Osuagwu-Nwogu <nnaemeka.osuagwu.n@gmail.com>
2023-06-22 06:08:06 +0000

    Debug 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,15 +10,33 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest = c;
+	int largest;
 
-	if (a > b && b > c)
+	if (a > b)
 	{
-		largest = a;
+		if (c > a)
+		{
+			largest = c;
+		}
+		else
+		{
+			largest = a;
+		}
 	}
-	else if (b > a && a > c)
+	else if (b > a)
 	{
-		largest = b;
+		if (c > b)
+		{
+			largest = c;
+		}
+		else
+		{
+			largest = b;
+		}
+	}
+	else
+	{
+		largest = c;
 	}
 
 	return (largest);
