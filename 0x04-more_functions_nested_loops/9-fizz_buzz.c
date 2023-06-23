7fafae1c3fd998a27027076aeb0ac6bf7e64fd6c
Nnaemeka Osuagwu-Nwogu <nnaemeka.osuagwu.n@gmail.com>
2023-06-23 03:37:08 +0000

    The Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,27 @@ int main(void)
 	{
 		if ((i % 15) == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if ((i % 5) == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if ((i % 3) == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
-		else
+		else if (i == 1)
 		{
 			printf("%d ", i);
 		}
+		else
+		{
+			printf(" %d", i);
+		}
 		i++;
 	}
+	printf("\n");
+	
 	return (0);
 }
