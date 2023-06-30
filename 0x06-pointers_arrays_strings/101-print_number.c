9bcba0f9697a2dfe8e6d8eff834e15640360a01c
Nnaemeka Osuagwu-Nwogu <nnaemeka.osuagwu.n@gmail.com>
2023-06-30 01:20:36 +0000

    A function that prints an integer using _putchar only

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,18 +7,20 @@
 
 void print_number(int n)
 {
+	int a;
+
 	if (n < 0)
 	{
+		a = -n;
 		_putchar('-');
-		n = -n;
 	}
 	else
 	{
-		n = n;
+		a = n;
 	}
-	if (n / 10)
+	if (a / 10)
 	{
-		print_number(n / 10);
+		print_number(a / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((a % 10) + '0');
 }
