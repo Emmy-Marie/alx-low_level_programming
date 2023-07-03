22a24d0cfdc7013386d60c042a8b5456b9e200d0
Nnaemeka Osuagwu-Nwogu <Nnaemeka.osuagwu.n@gmail.com>
2023-07-03 10:12:15 -0700

    A function that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strchr - A function that locates a character in a string
@@ -12,11 +11,6 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	if (s == NULL)
-	{
-		return (NULL);
-	}
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if ((unsigned char)s[i] == (unsigned char)c)
@@ -24,5 +18,5 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 	}
-	return (NULL);
+	return (0);
 }
