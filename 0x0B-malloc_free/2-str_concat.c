2a707990938836545fcc05c3fdcddd86a4401e89
Nnaemeka Osuagwu-Nwogu <nnaemeka.osuagwu.n@gmail.com>
2023-07-10 11:00:15 +0000

    A function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,48 +12,43 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	int a, b, len1, len2, total_len;
-
-	if (s1[1] == '\0')
+	int i, j, len1, len2, total_len;
+	/* check if s1 or s2 is NULL */
+	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-	if (s2[1] == '\0')
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
+	/* Calculate length of both strings and find the total */
 	len1 = 0;
 	while (s1[len1] != '\0')
 	{
 		len1++;
 	}
-
 	len2 = 0;
 	while (s2[len2] != '\0')
 	{
 		len2++;
 	}
-
 	total_len = len1 + len2;
-	ptr = malloc(sizeof(char) * (total_len + 1));
-
+	/* Allocation of memory to ptr and check for allocation fail */
+	ptr = malloc(sizeof(char) * (total_len + 1)); /* add null terminator */
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-
-	for (a = 0; a < len1; a++)
+	/* Concatenante both strings in ptr by iterating over all characters */
+	for (i = 0; i < len1; i++)
 	{
-		ptr[a] = s1[a];
+		ptr[i] = s1[i];
 	}
-
-	for (b = 0; b < len2; b++)
+	for (j = 0; j < len2; j++)
 	{
-		ptr[a] = s2[b];
-		a++;
+		ptr[i + j] = s2[j];
 	}
-	ptr[a] = '\0';
-
+	ptr[i + j] = '\0'; /* Add Null terminator to concatenated string */
 	return (ptr);
 }
