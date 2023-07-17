b43af6856e4cc846b14994626686a28f0d9dcf25
Nnaemeka Osuagwu-Nwogu <Nnaemeka.osuagwu.n@gmail.com>
2023-07-17 09:01:22 -0700

    A function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,12 +1,10 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
  * _strlen - A function that returns the length of a string
  * @s: char pointer variable
  * Return: (len) is the length of the string
  */
-
 int _strlen(char *s)
 {
 	int len;
@@ -19,7 +17,6 @@ int _strlen(char *s)
 	}
 	return (len);
 }
-
 /**
  * _strcpy - Copy the string pointed to by source to the buffer pointed
  *           by the destination.
@@ -27,7 +24,6 @@ int _strlen(char *s)
  * @src: Source pointer
  * Return: The destination pointer.
  */
-
 char *_strcpy(char *dest, char *src)
 {
 	char *origin_dest = dest;
@@ -42,7 +38,6 @@ char *_strcpy(char *dest, char *src)
 
 	return (origin_dest);
 }
-
 /**
  * new_dog - creates a new dog
  * @name: First element - name of the dog
@@ -58,29 +53,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	/* Calculate the length of name and owner strings */
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
-
 	/* Allocate memory for dog struct */
 	dog = malloc(sizeof(dog_t));
-
 	/* Check for allocation fail */
 	if (dog == NULL)
 	{
 		return (NULL);
 	}
-
 	/* Allocate memory for name string */
 	dog->name = malloc(sizeof(char) * (len1 + 1));
-
 	/* Check for allocation fail */
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-
 	/* Allocate memory for owner string */
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
-
 	/* Check for allocation fail and free if NULL */
 	if (dog->owner == NULL)
 	{
@@ -88,13 +77,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog->name);
 		return (NULL);
 	}
-
 	/* Copy name and owner strings to allocated memory */
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
-
 	/* Set the age of the dog */
 	dog->age = age;
-
 	return (dog);
 }
