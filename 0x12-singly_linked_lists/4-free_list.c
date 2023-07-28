5362116abe7baec426c5dc1b664dfc16ce152f40
Nnaemeka Osuagwu-Nwogu <Nnaemeka.osuagwu.n@gmail.com>
2023-07-27 19:22:18 -0700

    A function that frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/** free_list - A function that frees a linked list
+ * @head: The list to be freed
+ */
+
+void free_list(list_t *head)
+{
+	list_t *box;
+
+	while (head != NULL)
+	{
+		box = head->next;
+		free(head->str);
+		free(head);
+		head = box;
+	}
+}
